e15fbac2b60f66afdd0eb21732feaf91bff3c505
Nguyen Hai Dang <nh.dang.643264@gmail.com>
2025-04-21 12:06:17 +0700

    update

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,12 +1,12 @@
 import {
   Body,
   Controller,
-  Post,
-  UseGuards,
-  Request,
   Delete,
-  Param,
   Get,
+  Param,
+  Post,
+  Request,
+  UseGuards,
 } from '@nestjs/common';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
@@ -44,10 +44,55 @@ export class BookController {
     return this.bookService.requestLending(user, requestBookLendingDto);
   }
 
+  @Get('request-lending')
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(RolesGuard(UserRole.USER))
+  getLendingRequests(@Request() req: AuthRequest) {
+    const user = req.user;
+
+    return this.bookService.getLendingRequests(user);
+  }
+
   @Delete(':id')
   @UseGuards(JwtAuthGuard)
   @UseGuards(RolesGuard(UserRole.ADMIN))
   deleteBook(@Param('id') id: number) {
     return this.bookService.deleteBook(id);
+  }
+
+  // Admin endpoint for book
+  @Get('all-lending-status')
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(RolesGuard(UserRole.ADMIN))
+  getAllLendingStatus() {
+    return this.bookService.getAllLendingStatus();
+  }
+
+  @Get('requested-lending')
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(RolesGuard(UserRole.ADMIN))
+  getAllLendingRequests() {
+    return this.bookService.getAllLendingRequests();
+  }
+
+  @Post('approve-lending/:id')
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(RolesGuard(UserRole.ADMIN))
+  approveLending(@Param('id') id: number) {
+    return this.bookService.approveLending(id);
+  }
+
+  @Post('reject-lending/:id')
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(RolesGuard(UserRole.ADMIN))
+  rejectLending(@Param('id') id: number) {
+    return this.bookService.rejectLending(id);
+  }
+
+  @Post('return-lending/:id')
+  @UseGuards(JwtAuthGuard)
+  @UseGuards(RolesGuard(UserRole.ADMIN))
+  returnLending(@Param('id') id: number) {
+    return this.bookService.returnLending(id);
   }
 }
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Book } from './entities/book.entity';
 import { Repository } from 'typeorm';
@@ -17,12 +21,110 @@ export class BookService {
   ) {}
 
   async createBook(createBookDto: CreateBookDto) {
+    if (!createBookDto.stock || createBookDto.stock <= 0) {
+      createBookDto.stock = createBookDto.quantity;
+    }
     const book = this.bookRepository.create(createBookDto);
     return this.bookRepository.save(book);
   }
 
   async getAllBooks() {
     return this.bookRepository.find({});
+  }
+
+  async getAllLendingRequests() {
+    return this.bookLendingStatusRepository.find({
+      where: {
+        status: LendingStatus.REQUESTED,
+      },
+    });
+  }
+
+  async getLendingRequests(user: User) {
+    const lendingStatuses = await this.bookLendingStatusRepository.find({
+      where: {
+        user: { id: user.id },
+        status: LendingStatus.REQUESTED,
+      },
+      relations: ['book'],
+    });
+
+    return lendingStatuses;
+  }
+
+  async getAllLendingStatus() {
+    const res = await this.bookLendingStatusRepository.find({
+      relations: ['book', 'user'],
+    });
+
+    return res.map((lendingStatus) => ({
+      ...lendingStatus,
+      user: {
+        ...lendingStatus.user,
+        password: undefined,
+      },
+    }));
+  }
+
+  async approveLending(id: number) {
+    const lendingStatus = await this.bookLendingStatusRepository.findOne({
+      where: {
+        id,
+      },
+      relations: ['book'],
+    });
+
+    if (!lendingStatus) {
+      throw new NotFoundException('Lending request not found');
+    }
+
+    if (lendingStatus.status !== LendingStatus.REQUESTED) {
+      throw new NotFoundException('Lending request not in requested status');
+    }
+
+    const book = lendingStatus.book;
+
+    if (!book) {
+      throw new NotFoundException('Book not found');
+    }
+
+    if (book.stock < lendingStatus.quantity) {
+      throw new BadRequestException('Not enough stock available');
+    }
+
+    book.stock -= lendingStatus.quantity;
+    lendingStatus.status = LendingStatus.APPROVED;
+
+    await this.bookRepository.save(book);
+    await this.bookLendingStatusRepository.save(lendingStatus);
+
+    return {
+      message: 'Lending successfully',
+    };
+  }
+
+  async rejectLending(id: number) {
+    const lendingStatus = await this.bookLendingStatusRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!lendingStatus) {
+      throw new NotFoundException('Lending request not found');
+    }
+
+    if (lendingStatus.status !== LendingStatus.REQUESTED) {
+      throw new NotFoundException('Lending request not in requested status');
+    }
+
+    lendingStatus.status = LendingStatus.REJECTED;
+
+    await this.bookLendingStatusRepository.save(lendingStatus);
+
+    return {
+      message: 'Lending rejected successfully',
+    };
   }
 
   async requestLending(user: User, requestDto: RequestBookLendingDto) {
@@ -41,6 +143,38 @@ export class BookService {
     });
 
     return this.bookLendingStatusRepository.save(lendingStatus);
+  }
+
+  async returnLending(id: number) {
+    const lendingStatus = await this.bookLendingStatusRepository.findOne({
+      where: {
+        id,
+      },
+      relations: ['book'],
+    });
+
+    if (!lendingStatus) {
+      throw new NotFoundException('Lending request not found');
+    }
+
+    if (lendingStatus.status !== LendingStatus.APPROVED) {
+      throw new NotFoundException('Lending request not in approved status');
+    }
+
+    const book = lendingStatus.book;
+    if (!book) {
+      throw new NotFoundException('Book not found');
+    }
+
+    book.stock += lendingStatus.quantity;
+    lendingStatus.status = LendingStatus.RETURNED;
+
+    await this.bookRepository.save(book);
+    await this.bookLendingStatusRepository.save(lendingStatus);
+
+    return {
+      message: 'Book returned successfully',
+    };
   }
 
   async deleteBook(id: number) {
